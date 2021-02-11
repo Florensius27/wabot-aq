64d42db9aefcf8841f7b267272c5dc45d58a0a38
Florensius27 <78130347+Florensius27@users.noreply.github.com>
2021-02-11 14:24:20 +0700

    Update

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
     global.DATABASE._data.users[m.sender].exp += 500
-    m.reply('+500 XP')
+    m.reply('Anda telah mengclaim +500 XP')
     global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
-  } else m.reply('Anda sudah mengklaim klaim harian hari ini')
+  } else m.reply('_Anda sudah mengclaim *500 xp* sebelumnya!_')
 }
 handler.help = ['daily', 'claim']
 handler.tags = ['xp']
